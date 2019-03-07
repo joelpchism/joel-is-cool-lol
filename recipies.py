8e9a41b82243be426a837b77b1d9862e68681fb6
Jesse Wright <jessewright01@gmail.com>
2019-03-07 11:22:28 -0500

    Create recipies.py

---
+++ recipies.py
@@ -0,0 +1,10 @@
+recipies = {
+    "oatmeal": {
+        "ingredients":  ["2 cups water","1 cup oats","1/2 teaspoon salt"],
+        "steps": ["boil water", "stir"],
+    },
+    "pizza": {
+        "ingredients": ["flour", "tomato sauce", "egg", "salt"],
+        "steps": ["kneed dough", "bake"],
+    },
+}
